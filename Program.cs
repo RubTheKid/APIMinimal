ae57b0772dc120b98417f655041c78e73f77ede4
RubTheKid <you@example.com>
2022-10-31 18:45:29 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("TarefasDB"));
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapGet("yo kanye", async () => 
+    await new HttpClient().GetStringAsync("https://api.kanye.rest")
+    );
+
+app.MapGet("/tarefas", async (AppDbContext db) => await db.Tarefas.ToListAsync());
+
+app.MapPost("/tarefas", async (Tarefa tarefa, AppDbContext db) =>
+    {
+        db.Tarefas.Add(tarefa);
+        await db.SaveChangesAsync();
+        return Results.Created($"/tarefas/{tarefa.Id}", tarefa);
+    }
+);
+
+app.Run();
+
+class Tarefa
+{
+    public int Id { get; set; }
+    public string? Nome { get; set; }
+    public bool IsConcluida { get; set; }
+}
+
+class AppDbContext : DbContext 
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<Tarefa> Tarefas => Set<Tarefa>();
+
+}
